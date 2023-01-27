b3c845ae5a861d62c83481f9235275e3e6855fcf
Ankit Kumar <ankit.kr0048@gmail.com>
2023-01-27 20:52:20 +0530

    added Two Sum

---
+++ Leetcode/0001_Two_Sum.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> m = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!m.containsKey(target - nums[i]))
+                m.put(nums[i], i);
+            else
+                return new int[] { m.get(target - nums[i]), i };
+        }
+        return new int[] { -1, -1 };
+    }
+}
